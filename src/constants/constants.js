d4e026a926157d2491ca43da47e561556b89a410
Dumetella <dumetella@outlook.com>
2021-09-12 06:18:36 +0300

    added build

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'Websockets chatroom',
     description: "My take on chat application, using React, Node.js, Express, and Socket.io",
-    image: '/images/webchat.webp',
+    image: '/images/1.jpg',
     tags: ['Express', 'React', 'Node'],
     source: 'https://google.com',
     visit: 'https://google.com',
@@ -11,6 +11,8 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2020, text: 'Stopped playing League of Legends', },
-  { year: 2021, text: 'Decided to learn programming', },
+  { year: '31.05.21', text: 'Decided to learn programming', },
+  { year: '08.06.21', text: 'Learned Python as my first programming language', },
+  { year: '10.06.21', text: 'Learned basic HTML and CSS', },
+  { year: '30.06.21', text: 'Decided to switch from Python to JS', },
 ];
